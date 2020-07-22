6b73820e5c56bb03b67c3cbe18cdf21014263977
Tandi Sunarto <tandi.sunarto@hotmail.com>
2020-07-22 02:10:57 -0400

    Fix UI on the wasm project

--- BethanyPie/BethanysPieShopHRM.Wasm/Program.cs
+++ BethanyPie/BethanysPieShopHRM.Wasm/Program.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
+using BethanysPieShopHRM.Wasm.Services;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace BethanysPieShopHRM.Wasm
 {
@@ -17,7 +13,10 @@ namespace BethanysPieShopHRM.Wasm
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => {
+                client.BaseAddress = new Uri("https://localhost:44350/");
+            });
 
             await builder.Build().RunAsync();
         }
