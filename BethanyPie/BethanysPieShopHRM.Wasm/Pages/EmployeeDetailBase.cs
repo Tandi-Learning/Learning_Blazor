9c20b77f6e78c3e54061b0deced221dfb23b13d5
Tandi Sunarto <tandi.sunarto@hotmail.com>
2020-08-08 03:14:02 -0400

    Add js interop

--- BethanyPie/BethanysPieShopHRM.Wasm/Pages/EmployeeDetailBase.cs
+++ BethanyPie/BethanysPieShopHRM.Wasm/Pages/EmployeeDetailBase.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShopHRM.ComponentsLibrary.Map;
 
 namespace BethanysPieShopHRM.Wasm.Pages
 {
@@ -13,11 +14,28 @@ namespace BethanysPieShopHRM.Wasm.Pages
         [Parameter]
         public string EmployeeId { get; set; }
         public Employee Employee { get; set; } = new Employee();
+
+        public List<Marker> MapMarkers { get; set; } = new List<Marker>();
+
         [Inject]
         public IEmployeeDataService EmployeeDataService { get; set; }
         protected override async Task OnInitializedAsync()
         {
             Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+
+            System.Diagnostics.Debug.WriteLine(".........");
+            System.Diagnostics.Debug.WriteLine(Employee.Longitude);
+
+            MapMarkers = new List<Marker> 
+            {
+                new Marker 
+                {
+                    Description = $"{Employee.FirstName}  {Employee.LastName}",
+                    ShowPopup = false,
+                    X = Employee.Longitude,
+                    Y = Employee.Latitude
+                }
+            };
         }
 
     }
