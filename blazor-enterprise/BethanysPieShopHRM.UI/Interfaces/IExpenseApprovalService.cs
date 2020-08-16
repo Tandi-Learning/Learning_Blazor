00b2818cd6de51e21f23d7aff0a534f6566e798f
Tandi Sunarto <tandi.sunarto@hotmail.com>
2020-08-16 02:02:15 -0400

    Refactor to use DI

---
+++ blazor-enterprise/BethanysPieShopHRM.UI/Interfaces/IExpenseApprovalService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using BethanysPieShopHRM.Shared;
+
+namespace BethanysPieShopHRM.UI.Services
+{
+    public interface IExpenseApprovalService
+    {
+        Task<ExpenseStatus> GetExpenseStatus(Expense expense);
+    }
+}
---
+++ blazor-enterprise/BethanysPieShopHRM.UI/Services/ExpenseApprovalService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using BethanysPieShopHRM.Shared;
+
+namespace BethanysPieShopHRM.UI.Services
+{
+    public class ExpenseApprovalService : IExpenseApprovalService
+    {
+        private IEmployeeDataService _employeeDataService { get; set; }
+
+        public ExpenseApprovalService(IEmployeeDataService EmployeeDataService)
+        {
+            _employeeDataService = EmployeeDataService;
+        }
+
+        public async Task<ExpenseStatus> GetExpenseStatus(Expense expense)
+        {
+            var employee = await _employeeDataService.GetEmployeeDetails(expense.EmployeeId);
+
+            if (employee.IsOPEX)
+            {
+                switch (expense.ExpenseType)
+                {
+                    case ExpenseType.Conference:
+                    case ExpenseType.Transportation:
+                    case ExpenseType.Hotel:
+                    case ExpenseType.Food:
+                        return ExpenseStatus.Denied;
+                }
+
+                if (expense.Status != ExpenseStatus.Denied)
+                {
+                    expense.CoveredAmount = expense.Amount / 2;
+                }
+            }
+
+            if (!employee.IsFTE)
+            {
+                if (expense.ExpenseType != ExpenseType.Training)
+                {
+                    return ExpenseStatus.Denied;
+                }
+            }
+
+            if (expense.ExpenseType == ExpenseType.Food && expense.Amount > 100)
+            {
+                return ExpenseStatus.Pending;
+            }
+
+            if (expense.Amount > 5000)
+            {
+                return ExpenseStatus.Pending;
+            }
+
+            return ExpenseStatus.Pending;
+        }
+    }
+}
